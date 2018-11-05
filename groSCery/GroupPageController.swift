ca5f2f10b5f2c44636255e9b0556a4d2a67d92fa
Naman Kedia <naman.kedia27@gmail.com>
2018-11-04 22:01:24 -0800

    Added Tab Bar to toggle between users list of items and groups list of items. Implemented Group List View Controller.

--- groSCery/GroupPageController.swift
+++ groSCery/GroupPageController.swift
@@ -8,23 +8,105 @@
 
 import UIKit
 
-class GroupPageController: UIViewController {
+class GroupPageController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var user: User!
+    var allItems = [Item]()
+    var suscribedItems = [Item]()
+    var notSuscribedItems = [Item]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        suscribedItems.append(Item(inStock: true, name: "Eggs", suscribedUsers: ["1"]))
+        suscribedItems.append(Item(inStock: true, name: "Banana", suscribedUsers: ["1"]))
+        suscribedItems.append(Item(inStock: false, name: "Apple", suscribedUsers: ["1"]))
+        suscribedItems.append(Item(inStock: true, name: "Milk", suscribedUsers: ["1"]))
+        suscribedItems.append(Item(inStock: false, name: "Water", suscribedUsers: ["1"]))
+        suscribedItems.append(Item(inStock: true, name: "Cereal", suscribedUsers: ["1"]))
+        
+        notSuscribedItems.append(Item(inStock: true, name: "Cookies", suscribedUsers: ["1"]))
+        notSuscribedItems.append(Item(inStock: false, name: "Toilet Paper", suscribedUsers: ["1"]))
+        notSuscribedItems.append(Item(inStock: false, name: "Orange Juice", suscribedUsers: ["1"]))
+
     }
     
-//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-//        if segue.identifier == "mySegue"{
-//            let vc = segue.destinationViewController as! GroupPageController
-//        }
-//    }
-
-//    @IBAction func GoToUser(_ sender: Any) {
-//        performSegue(withIdentifier: "GroceryListToBoughtItemSegueID", sender: Any.self)
-//
-//    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (section == 0) {
+            return "Not Suscribed"
+        } else {
+            return "Suscribed"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 0) {
+            return notSuscribedItems.count
+        } else {
+            return suscribedItems.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "GroupItemsCellID")
+        var item = Item(inStock: true, name: "", suscribedUsers: [])
+        if (indexPath.section == 0) {
+            item = notSuscribedItems[indexPath.row]
+        } else {
+            item = suscribedItems[indexPath.row]
+        }
+        cell.textLabel?.text = item.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (indexPath.section == 0) {
+            let item = notSuscribedItems[indexPath.row]
+            let alert = UIAlertController(title: item.name, message: "Suscribe to \(item.name)", preferredStyle: UIAlertController.Style.alert)
+            let yesAction = UIAlertAction(title: "Yes", style: .default, handler: { (action) -> Void in
+                print("ACTION 3 selected!")
+                self.suscribeToItem(item: item, index: indexPath.row)
+                
+            })
+            alert.addAction(yesAction)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let item = suscribedItems[indexPath.row]
+            let alert = UIAlertController(title: item.name, message: "Unsuscribe to \(item.name)", preferredStyle: UIAlertController.Style.alert)
+            let yesAction = UIAlertAction(title: "Yes", style: .default, handler: { (action) -> Void in
+                print("ACTION 3 selected!")
+                self.unsuscribeItem(item: item, index: indexPath.row)
+            })
+            alert.addAction(yesAction)
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+    }
+    
+    func suscribeToItem(item: Item, index: Int) {
+        suscribedItems.append(item)
+        notSuscribedItems.remove(at: index)
+        tableView.reloadData()
+    }
+    
+    func unsuscribeItem(item: Item, index: Int) {
+        suscribedItems.remove(at: index)
+        notSuscribedItems.append(item)
+        tableView.reloadData()
+    }
+    
+    
     
     
 }
